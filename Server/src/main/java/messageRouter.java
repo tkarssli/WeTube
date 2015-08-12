03077858043c617e071d31a7af95dd5e84d238c4
Tamir Karssli <tkarssli@gmail.com>
2015-08-11 18:58:54 -0700

    fixed bad naming convention

--- Server/src/main/java/messageRouter.java
+++ Server/src/main/java/messageRouter.java
@@ -1,5 +1,35 @@
+import com.corundumstudio.socketio.SocketIOServer;
+import handlers.UserHandler;
+import objects.Message;
+import objects.UserObject;
+
 /**
  * Created by Tamir on 8/11/2015.
+ *
+ * Class for passing commands between users
  */
-public class messageRouter {
+public class MessageRouter {
+    private final UserHandler userHandler;
+    private final SocketIOServer server;
+
+    public MessageRouter(UserHandler userHandler, SocketIOServer server) {
+        this.userHandler = userHandler;
+        this.server = server;
+    }
+
+    public void route(Message message) {
+
+        // Get origin and target
+        UserObject user1 = userHandler.getUser(message.origin);
+        UserObject user2 = userHandler.getUser(message.target);
+
+        String command = message.command;
+
+        // Pass message from user1 to user2
+        server.getClient(user1.getSessionId()).sendEvent("message", message);
+
+        System.out.println(command + " command sent from " + user1.getUserName() + " to " + user2.getUserName());
+
+
+    }
 }
